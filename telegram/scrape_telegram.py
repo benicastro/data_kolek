21b74249a88631d2a340cc91869682dda2b9f043
benicastro <benedict.zcastro@gmail.com>
2024-03-19 20:50:05 +0800

    added the import of modules and the dataframe schema in the script

--- telegram/scrape_telegram.py
+++ telegram/scrape_telegram.py
@@ -1,1 +1,22 @@
+# Import telegram sources from file
+from telegram_source import telegram_links
 
+# Import modules/libraries
+import requests
+import pandas as pd
+from datetime import *
+from dataclasses import dataclass, asdict
+
+
+# Create dataframe schema
+@dataclass
+class PostInfo:
+    channel_name: str
+    channel_label: str
+    username: str
+    username_link: str
+    post_text: str
+    post_link: str
+    date: str
+    number_of_views: int | None
+    extracted_urls: list | None
