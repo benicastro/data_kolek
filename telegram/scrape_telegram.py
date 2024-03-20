00d64c78aecd4c70158eccc1dd1fbbf2c2788a1a
benicastro <benedict.zcastro@gmail.com>
2024-03-21 01:49:06 +0800

    worked on scraping texts from posts in a telegram channel

--- telegram/scrape_telegram.py
+++ telegram/scrape_telegram.py
@@ -49,7 +49,6 @@ class TelegramScraper:
     def scrape_channel(self, channel_name, channel_label):
         posts_list = []
         continue_scraping = True
-        post_number_before = None
         query_params = {"before": None}
 
         # # Restrict the number of scraped items
@@ -81,15 +80,59 @@ class TelegramScraper:
                 return
             post_number = post_number_query[1]
 
-            if post_number == post_number_before:
+            if post_number == query_params["before"]:
                 print("All posts already scraped. Moving on to next one...")
                 continue_scraping = False
                 continue
             query_params["before"] = post_number
-            print(post_number)
 
-            if int(post_number) < 1800:
+            print(f"Scraping posts from batch identifier - {post_number}...")
+
+            posts = parsed_html.css(
+                "div.tgme_widget_message_wrap.js-widget_message_wrap"
+            )
+
+            if not posts:
+                print("[!] No posts found.")
                 return
+
+            for i, post in enumerate(posts):
+
+                try:
+                    text_node = post.css_first(
+                        "div.tgme_widget_message_text.js-message_text"
+                    )
+                    if text_node is None:
+                        continue
+                    else:
+                        post_text = text_node.text()
+                except Exception as err:
+                    post_text = None
+
+                username = ""
+                username_link = ""
+                post_link = ""
+                date = ""
+                number_of_views = 0
+                extracted_urls = []
+
+                entry = self.get_entry(
+                    channel_name=channel_name,
+                    channel_label=channel_label,
+                    username=username,
+                    username_link=username_link,
+                    post_text=post_text,
+                    post_link=post_link,
+                    date=date,
+                    number_of_views=number_of_views,
+                    extracted_urls=extracted_urls,
+                )
+
+                posts_list.append(asdict(entry))
+
+                if int(post_number) < 1800:
+                    print(f"{len(posts_list)} posts returned.")
+                    return posts_list
 
     def make_request(self, endpoint, query_params):
         for i in range(3):
@@ -125,6 +168,32 @@ class TelegramScraper:
             print("[!] {telegram_link} is not a valid telegram link - skipping...")
             return None
 
+    def get_entry(
+        self,
+        channel_name,
+        channel_label,
+        username,
+        username_link,
+        post_text,
+        post_link,
+        date,
+        number_of_views,
+        extracted_urls,
+    ):
+        """This function creates an entry for the output data with the recommended schema using information from the website."""
+        new_entry = PostInfo(
+            channel_name=channel_name,
+            channel_label=channel_label,
+            username=username,
+            username_link=username_link,
+            post_text=post_text,
+            post_link=post_link,
+            date=date,
+            number_of_views=number_of_views,
+            extracted_urls=extracted_urls,
+        )
+        return new_entry
+
     def get_results(self):
         for source in self.sources:
             print(f"Scraping {source.get('url')}...")
@@ -137,7 +206,11 @@ class TelegramScraper:
                 channel_info["channel_name"], channel_info["channel_label"]
             )
 
+        return channel_content
+
 
 # Test
 telegram_scraper = TelegramScraper([telegram_links[0]])
-telegram_scraper.get_results()
+scraped_data = telegram_scraper.get_results()
+df = pd.DataFrame(scraped_data)
+print(df)
