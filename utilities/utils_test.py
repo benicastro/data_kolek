835546d38d66f544fa86f2012b3f644bc6eb332e
benicastro <benedict.zcastro@gmail.com>
2024-04-02 22:53:33 +0800

    tested the extract_domain function in the utils script

--- utilities/utils_test.py
+++ utilities/utils_test.py
@@ -1,1 +1,20 @@
 # TEST FILE #
+
+# Import Files
+import utils as utils
+
+# Test extract_domain function
+
+sample_urls = [
+    "www.abc.au.uk",
+    "https://github.com",
+    "http://github.ca",
+    "https://www.google.ru",
+    "http://www.google.co.uk",
+    "www.yandex.com",
+    "yandex.ru",
+]
+
+for url in sample_urls:
+    domain = utils.extract_domain(url)
+    print(domain)
